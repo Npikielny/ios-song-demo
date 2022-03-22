d4f4195032a6a02e68ceebd95e80764a1631805e
Noah Pikielny <np299@cornell.edu>
2022-03-22 12:07:25 -0400

    Configured Heroku Database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,13 +7,23 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    guard let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) else { throw Abort(.internalServerError) }
+    
+    
+    postgresConfig.tlsConfiguration = .makeClientConfiguration()
+    postgresConfig.tlsConfiguration?.certificateVerification = .none
     app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        configuration: postgresConfig
     ), as: .psql)
+    
+//    app.databases.use(.postgres(
+//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+//        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+//        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+//        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+//    ), as: .psql)
+    
 
     app.migrations.add(CreateSong())
     try app.autoMigrate().wait()
